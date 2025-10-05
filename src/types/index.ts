3bb5afc84c48ad3bc8a48c59a5556b62c291b3cb
Michael <designare.design@gmail.com>
2025-10-05 13:01:07 +0200

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,10 @@
-
+// Definiert die Struktur eines Benutzer-Objekts, wie es in der Datenbank gespeichert wird.
+export interface User {
+  id: string;
+  email: string;
+  password?: string; // Das Passwort sollte nie an das Frontend gesendet werden
+  role: 'SUPERADMIN' | 'ADMIN' | 'BENUTZER';
+  domain?: string;
+  createdByAdminId?: string;
+  createdAt: Date;
+}
