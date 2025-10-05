0ded1e7676e1bac1cdac99e8131681aea88072d8
Michael <designare.design@gmail.com>
2025-10-05 19:32:35 +0200

    Update next-auth.d.ts

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,30 +1,25 @@
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+// src/next-auth.d.ts
+
+import { DefaultSession, DefaultUser } from "next-auth";
 import { JWT, DefaultJWT } from "next-auth/jwt";
-import { User as CustomUser } from "."; // Importiert unseren User-Typ aus index.ts
+import { User as CustomUser } from "./types";
 
 declare module "next-auth" {
-  /**
-   * Erweitert den Session-Typ, um unsere 'role' Eigenschaft aufzunehmen.
-   */
   interface Session {
     user: {
+      id: string; // Stellt sicher, dass 'id' im Session-User-Objekt bekannt ist
       role: CustomUser['role'];
     } & DefaultSession["user"];
   }
 
-  /**
-   * Erweitert den User-Typ, der vom authorize-Callback zurückgegeben wird.
-   */
   interface User extends DefaultUser {
     role: CustomUser['role'];
   }
 }
 
 declare module "next-auth/jwt" {
-  /**
-   * Erweitert den JWT-Token-Typ.
-   */
   interface JWT extends DefaultJWT {
+    id: string; // Stellt sicher, dass 'id' im JWT-Token bekannt ist
     role: CustomUser['role'];
   }
 }
