3930a5ee3868a9e4885b7a23e691063a7e27aa36
Michael <designare.design@gmail.com>
2025-10-05 10:03:11 +0200

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,4 @@
 import React from 'react';
-
-// Jede Layout-Datei muss die "children" Eigenschaft annehmen.
-// "children" ist der Inhalt der page.tsx, der hier eingesetzt wird.
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <section>
-      {/* Hier könnte später eine gemeinsame Navigation für das Dashboard hinkommen */}
-      <nav>Dashboard Navigation</nav>
-      
-      {/* Hier wird der Inhalt der Seite (page.tsx) angezeigt */}
-      {children}
-    </section>
-  );
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
 }
