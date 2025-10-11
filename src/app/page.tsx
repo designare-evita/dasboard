351b732232773676d2665dae189464a7c2dc3dd9
Michael <designare.design@gmail.com>
2025-10-11 17:52:51 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 
 import { useSession } from "next-auth/react";
 import useApiData from "@/hooks/use-api-data";
-import Header from "@/components/layout/Header";
 import KpiCard from "@/components/kpi-card";
 import Link from "next/link";
 
@@ -30,7 +29,6 @@ export default function DashboardPage() {
 
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
-      <Header />
       <main className="mt-6">
         {error && <div className="p-6 text-center bg-red-100 rounded-lg text-red-700">{error}</div>}
 
