5b7bfe04c831cc47d0c3a9460e07a94724f5dc98
Michael <designare.design@gmail.com>
2025-10-17 16:20:27 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,4 @@
 export { default } from "next-auth/middleware"
-
-// Hier legen wir fest, welche Seiten geschützt werden sollen.
-// Der "matcher" sorgt dafür, dass die Middleware auf diesen Routen läuft.
 export const config = { 
   matcher: [
     /*
