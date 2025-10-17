d143cffda13de309bf4a20a10120ca39df8dfd59
Michael <designare.design@gmail.com>
2025-10-17 09:46:57 +0200

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        // HIER IST DEINE ÄNDERUNG:
+        default: "border border-gray-300 text-gray-600 hover:bg-gray-100",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -21,7 +22,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        // HIER IST DEINE ÄNDERUNG:
+        default: "h-9 px-3 py-1",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
