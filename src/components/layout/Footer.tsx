82f2c9c58c0f80a7d4321b199693c4a139c9266a
Michael <designare.design@gmail.com>
2025-10-17 15:35:14 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,8 +7,10 @@ const Footer = () => {
 
   return (
     <footer className="w-full bg-white shadow-inner mt-12 py-6">
-      <div className="max-w-7xl mx-auto px-4 text-center text-gray-500 text-xs">
-        <p>&copy; {currentYear} Michael Kanda & Evita. Alle Rechte vorbehalten.</p>
+      <div className="max-w-7xl mx-auto px-4 text-center text-gray-500 text-s">
+        <p>
+          &copy; {currentYear} <a href="https://designare.at/" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-700">Michael Kanda & Evita</a>. Jede Codezeile von Hand gestreichelt. Also bitte nicht klauen. Alle Rechte vorbehalten.
+        </p>
       </div>
     </footer>
   );
