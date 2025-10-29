aff921b575362b000cb61aa0a4e095154600795b
Michael <designare.design@gmail.com>
2025-10-29 18:11:28 +0100

    Update EditUserForm.tsx

--- src/app/admin/edit/[id]/EditUserForm.tsx
+++ src/app/admin/edit/[id]/EditUserForm.tsx
@@ -238,6 +238,8 @@ export default function EditUserForm({ user, onUserUpdated }: EditUserFormProps)
             </div>
 
             {/* ========== SEMRUSH SECTION ========== */}
+            <fieldset className="border-t pt-4 mt-4">
+              <legend className="text-sm font-bold text-gray-900 mb-3">📊 Semrush Einstellungen</legend>
 
               {/* Semrush Projekt ID */}
               <div>
