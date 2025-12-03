276113d7518ead01009169ab4e344b0d8c9d4337
Michael <designare.design@gmail.com>
2025-12-03 19:27:22 +0100

    Update ProjectDashboard.tsx

--- src/components/ProjectDashboard.tsx
+++ src/components/ProjectDashboard.tsx
@@ -82,7 +82,7 @@ export default function ProjectDashboard({
         userEmail={userEmail}
         showDateSelector={true}
         dateRange={dateRange}
-        onDateRangeChange={onDateRangeChange}
+        onDateRangeChange={onDateRangeChange || (() => {})}
       />
 
       <main className="max-w-[1600px] mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-8">
